7211011a340b117447c7d08f90d4e14a9641990b
Anastasiia <asya07200@gmail.com>
2021-08-04 13:01:20 +0300

    Реализована адаптивная верстка основного компонента-контейнера приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './styles/App.css'
 import { getWeatherForWeek, getWeatherFromPast } from './dataSources/index'
 import { APIKey } from './dataSources/common'
 import { WeatherAppPage } from './pages/WeatherAppPage'
--- src/pages/WeatherAppPage.tsx
+++ src/pages/WeatherAppPage.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import { BlockData } from '../components/blockData/BlockData'
-import './weatherAppPage.css'
-import logo from '../images/logo.png'
+import '../styles/weatherAppPage.css'
 
 export function WeatherAppPage () {
   return (
-    <div className="weatherAppPage">
-      <div className="weatherAppPage__logo">
-        <img src={logo} alt="logo"></img>
+    <div className="weather-app">
+      <header className="weather-app__header">
+        <div className="weather-app__header-title">
+          <p className="weather-app__header-title-weather">Weather</p>
+          <p className="weather-app__header-title-forecast">forecast</p>
+        </div>
+      </header>
+      <main className="weather-app__main">
+        <div className="weather-app__white-box">
+        <h1 className="weather-app__white-box-title">7 Days Forecast</h1>
+        <p></p>
       </div>
-      <div className="weatherAppPage__whiteBoxes">
-        <BlockData />
+      <div className="weather-app__white-box">
+        <h1 className="weather-app__white-box-title">Forecast for a Date in the Past</h1>
       </div>
-
+      </main>
+      <footer className="weather-app__footer">
+        <span className="weather-app__footer-copyright">с любовью от mercury development</span>
+      </footer>
     </div>
   )
 }
