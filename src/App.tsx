f107bb6f2857de827b059e9f97bafc9c3d9e8cae
Anastasiia <asya07200@gmail.com>
2021-09-09 12:06:54 +0300

    Реализована функция расчета font-size

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { WeatherAppPage } from './pages/WeatherAppPage'
 
 function App () {
+  const htmlRef = useRef<HTMLElement | null>(null)
+
+  const handleResize = (): void => {
+    if (htmlRef.current === null) {
+      return
+    }
+
+    const { width, height } = htmlRef.current.getBoundingClientRect()
+
+    let baseFontSize = width / 1440
+
+    if (width >= 1440) {
+      baseFontSize = Math.min(width / 1440, height / 960)
+    } else if (width >= 1028 && width < 1440) {
+      baseFontSize = Math.min(width / 1028, height / 768)
+    } else {
+      baseFontSize = Math.min(width / 320, height / 667)
+    }
+
+    htmlRef.current.style.cssText = `font-size: ${baseFontSize}px;`
+  }
+
+  useEffect(() => {
+    htmlRef.current = document.documentElement
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return (): void => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <div className="App">
       <header></header>
-      <body>
+      <div>
         <WeatherAppPage />
-      </body>
+      </div>
     </div>
   )
 }
